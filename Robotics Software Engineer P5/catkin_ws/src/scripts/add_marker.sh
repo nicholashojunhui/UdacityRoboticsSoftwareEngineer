84cb3773cf45a81061a3306276de2fe203fba408
nicholashojunhui <37932759+nicholashojunhui@users.noreply.github.com>
2026-03-31 09:59:38 +0800

    Use variables for map and RViz paths in add_marker.sh

--- Robotics Software Engineer P5/catkin_ws/src/scripts/add_marker.sh
+++ Robotics Software Engineer P5/catkin_ws/src/scripts/add_marker.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+MAP_PATH=$(rospack find add_markers)/../map/TB3_WORLD.yaml
+RVIZ_PATH=$(rospack find add_markers)/../rvizConfig/home_service_rvizConfig.rviz
+
 xterm -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
 sleep 5
 
-xterm -e "roslaunch add_markers amcl.launch map_file:=$HOME/catkin_ws/src/map/TB3_WORLD.yaml" &
+xterm -e "roslaunch add_markers amcl.launch map_file:=$MAP_PATH" &
 sleep 5
 
-xterm -e "roslaunch add_markers view_home_service_navigation.launch rviz_path:=$HOME/catkin_ws/src/rvizConfig/home_service_rvizConfig.rviz" &
+xterm -e "roslaunch add_markers view_home_service_navigation.launch rviz_path:=$RVIZ_PATH" &
 sleep 5
 
 xterm -e "rosrun add_markers initial_pose2.py" &
